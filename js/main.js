3aa7756d5d8df8319e1ed8b55e51a410464c3b7a
benjamin <degroote.benjamin@gmail.com>
2017-07-15 16:59:31 +0200

    select img work with bug

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
 // var init
 var tab = ["as1.jpg","as2.png","as3.png","as4.png","as1.jpg","as2.png","as3.png","as4.png","2c.jpg","2carreau.png","2t.jpg","2c.jpg","2carreau.png","2t.jpg"]
 var img = document.getElementsByTagName("img")
+var resultat = []
 
 for(let i=0;i<img.length;i++){
 img[i].addEventListener("click", function() {
   img[i].src = "img/" + tab[i]
+  resultat.push(tab[i])
+  if(resultat.length === 2){
+      setTimeout(bonjour,1000)
+  }
 })};
+
+function bonjour(){
+    if(resultat[0] !== resultat[1]){
+      for(let y=0;y<img.length;y++){
+        if(img[y].src === ("file:///home/benjamin/site/lab/memory/img/" + resultat[0]) || img[y].src === ("file:///home/benjamin/site/lab/memory/img/" + resultat[1])){
+          img[y].src = ""
+        }
+      }
+      resultat.length = 0
+    }
+    else {
+      resultat.length = 0
+    }
+}
